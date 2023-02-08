16e798fc1dcbc82d20e4c7a5c748dca908372001
Nikolai Stukalo <nist2902@yandex.ru>
2023-02-08 15:33:00 +0300

    Correct small errors

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '13.0'
+
+target 'Simple Chat App' do
+  use_frameworks!
+
+  # Pods for Chat App
+
+pod 'CLTypingLabel', '~> 0.4.0'
+pod 'Firebase/Auth'
+pod 'Firebase/Firestore'
+pod 'IQKeyboardManagerSwift'
+
+end
---
+++ Simple Chat App/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  Firebase Chat
+//
+//  Created by Николай Стукало on 06.12.2022.
+//
+
+struct K {
+    static let appName = "⚡️Chat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
+
