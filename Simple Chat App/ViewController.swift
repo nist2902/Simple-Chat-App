4144dbe5d368f5446c284ac4308adcebb86edd21
Nikolai Stukalo <nist2902@yandex.ru>
2023-02-08 14:14:52 +0300

    Initial Commit

---
+++ Simple Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Chat App
+//
+//  Created by Николай Стукало on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
