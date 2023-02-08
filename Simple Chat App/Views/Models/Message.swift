53622a7d9d3f959f37e2a2ef80880232340e3058
Nikolai Stukalo <nist2902@yandex.ru>
2023-02-08 15:10:46 +0300

    UI adjustment.

---
+++ Simple Chat App/Views/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Simple Chat App
+//
+//  Created by Николай Стукало on 08.02.2023.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
